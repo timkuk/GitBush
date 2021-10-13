23b2bd43549e2cd7a4b30810413c5e4a7eb03df4
=Kurchenok Tim <tim_fedorenko@mail.ru>
2021-10-13 23:41:19 +0300

    Good

--- TestSolution/TestProject/Program.cs
+++ TestSolution/TestProject/Program.cs
@@ -7,6 +7,7 @@ namespace TestProject
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("You can do it!");
         }
     }
 }
